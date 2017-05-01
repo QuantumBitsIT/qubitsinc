a4c844b80ac68b34c19a12088860a6e680529eaa
QuantumBitsIT <beachd80@gmail.com>
2017-05-01 13:40:37 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Hello Everyone</title>
+    </head>
+
+    <body>
+<div align="center">
+                <h1>This app is created and maintained by QubitsInc</h1>
+                <h2>This is a test application run by AWS Opsworks</h2>
+                <div align="center"><img src="https://s3-us-west-2.amazonaws.com/cdscloud-davidbeach/halstatt+06.jpg"></div>
+                <p>OpsWorks Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
+                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+        </div>
+    </body>
+
+</html>
